38b859554fc073075cdbb178274e39e6490330b1
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2024-10-12 17:06:31 +0200

    clase 12-10

---
+++ clase4-12-10-24.py
@@ -0,0 +1,46 @@
+#Datos estructurados
+
+lista=[1,2,3,4,5]
+numero1=lista[0]
+numero2=lista[1]
+numero3=lista[2]
+numero4=lista[3]
+numuero4=lista[4]
+
+print(type(numero1), type(numero2), type(numero3),type(numero4),)
+
+print(type(lista))
+
+
+
+
+
+tupla=(1,2,3,4,5)
+print(type(tupla))
+
+
+
+
+
+lista=[1,2,3,4,5]
+# Imprima por pantalla el último numero de la lista
+print(lista[4])
+
+# Cambie el numero de la primera posición por el numero 2
+lista=[2,2,3,4,5]
+print(lista)
+
+#alternativa
+lista=[1,2,3,4,5]
+lista[0]=2
+print(lista)
+
+
+#Len: sirve para saber la extensión de la lista
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+lista=[1,2,3,4,5]
+print(len(lista))
+
+
+#
